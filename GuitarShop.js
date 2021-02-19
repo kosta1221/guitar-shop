786804341f571891c9aea9c04e28effb5e13d141
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-02-19 21:45:20 +0200

    Add constructor
    Add constructor function for ClassicGuitar objects

---
+++ GuitarShop.js
@@ -0,0 +1,8 @@
+//Constructor function for ClassicGuitar objects
+function ClassicGuitar(manufactureYear, brand, price) {
+	this.manufactureYear = manufactureYear;
+	this.brand = brand;
+	this.price = price;
+	this.numberOfStrings = 6;
+	this.used = false;
+}
