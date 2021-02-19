d67a832f97b7baeb7d7678decdb1f07c1c219ee3
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-02-19 22:17:43 +0200

    Add methods + static method detectSound

--- GuitarShop.js
+++ GuitarShop.js
@@ -5,4 +5,33 @@ function ClassicGuitar(manufactureYear, brand, price) {
 	this.price = price;
 	this.numberOfStrings = 6;
 	this.used = false;
+
+	this.play = function () {
+		this.price *= 0.9;
+		return "🎶🎶🎶";
+	};
+
+	this.getManufactureYear = function () {
+		return this.getManufactureYear;
+	};
+
+	this.getBrand = function () {
+		return this.brand;
+	};
+
+	this.getPrice = function () {
+		return this.price;
+	};
+
+	this.setPrice = function (newPrice) {
+		this.price = newPrice;
+	};
+}
+
+function detectSound(sound) {
+	if (sound === "🎸") {
+		return "ElectricGuitar";
+	} else if (sound === "🔊") {
+		return "BassGuitar";
+	}
 }
